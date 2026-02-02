13aa40188f66c193b5948a0c0e86726ebbe26ebf
Mohammed Kaif <kaifmohammed008@gmail.com>
2026-02-02 20:59:17 +0530

    deployment modifications

--- src/api/axios.config.ts
+++ src/api/axios.config.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Your Spring Boot port
+  baseURL: API_BASE_URL + '/api', // Your Spring Boot port
   headers: {
     'Content-Type': 'application/json',
   },
